15605269c13d4d84f7f694a641f43b960d9d8688
Joseph Joseph <josephnjoseph43@gmail.com>
2023-11-16 21:40:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import replicate
+import os
+from flask import Flask
+app = Flask(__name__)
+
+os.environ['REPLICATE_API_TOKEN'] = 'r8_I6byr460oRsgfcmkJELYfHtl9VACL8d1QsffB'
+@app.route("/api/<word>")
+def api(word):
+  output = replicate.run(
+  "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+  input={
+    "debug": False,
+    "top_k": 50,
+    "top_p": 1,
+    "prompt": "Tell about gaganyaan mission",
+    "temperature": 0.5,
+    "system_prompt": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+    "max_new_tokens": 500,
+    "min_new_tokens": -1
+    }
+  )
+  texter=""
+  for item in output:
+       texter+=item
+  return texter
+if __name__=="__main__":
+    app.run()
