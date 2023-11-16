e0143586d6e6a8cdad38e79cb109ff36bbce81f8
Joseph Joseph <josephnjoseph43@gmail.com>
2023-11-17 05:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def api(word):
     "debug": False,
     "top_k": 50,
     "top_p": 1,
-    "prompt": "Tell about gaganyaan mission",
+    "prompt": word,
     "temperature": 0.5,
     "system_prompt": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
     "max_new_tokens": 500,
